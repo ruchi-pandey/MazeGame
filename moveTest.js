c5034fecdf7234a67f60ce56b24799471deeaa9f
ru <ruchisppandey@gmail.com>
2022-09-27 19:51:52 +0530

    Fixed the glitch - removing old frames

--- moveTest.js
+++ moveTest.js
@@ -1,80 +1,84 @@
 
 var imageRepo= new function(){
 
-this.player =new Image();
-this.player.src='https://opengameart.org/sites/default/files/Green-Cap-Character-16x18.png';
-
-this.bg =new Image();
-this.bg.src="maze.jpg";
-
-}
-
-let player = document.getElementById('player');
-let ctx=player.getContext('2d');
-
-const scale = 1.5;
-const width = 16;
-const height = 18;
-const scaledHeight = scale*height;
-const scaledWidth = scale*width;
-
-function drawFrame(frameX, frameY, canvasX, canvasY){
-  ctx.drawImage(imageRepo.player,
-               frameX* width, frameY * height, width, height,
-               canvasX, canvasY, scaledWidth, scaledHeight);
-}
-
-function background(){
-  ctx.drawImage(imageRepo.bg,0,0)
-}
-
-/*
-img - imgrepo.img
-sx - fX*w - o
-sy - fY*h - o
-swidth - w - o
-sheight - h - o
-x - cX - 160
-y - cY - 35
-width - sW - o
-height - sH - o
-*/
-
-function init(){
-  this.background();
-  this.drawFrame(0,0, 165, 35);
-  this.playerMovement();
+  this.player =new Image();
+  this.player.src='https://opengameart.org/sites/default/files/Green-Cap-Character-16x18.png';
   
-}
-
-xPos=165;
-yPos=50;
-
-document.addEventListener('keydown',playerMovement) 
-
-function playerMovement(e){
-  var x = e.keyCode;
-  drawFrame(0,0, xPos, yPos);
- 
-
-  if(x == 39){
+  this.bg =new Image();
+  this.bg.src="maze.jpg";
   
-    xPos+=10;
   }
 
-  if(x == 37){
+  
+
+  let player = document.getElementById('player');
+  let ctx=player.getContext('2d');
+
+  let bgMaze = document.getElementById('background');
+  let bgCtx = bgMaze.getContext('2d');
+  
+  
+  const scale = 1.5;
+  const width = 16;
+  const height = 18;
+  const scaledHeight = scale*height;
+  const scaledWidth = scale*width;
+  
+  function background(){
+    bgCtx.drawImage(imageRepo.bg,0,0)
+  }
+
+  function drawFrame(frameX, frameY, canvasX, canvasY){
+      ctx.drawImage(imageRepo.player,
+                   frameX* width, frameY * height, width, height,
+                   canvasX, canvasY, scaledWidth, scaledHeight);
+    }
+  
+  function init(){
+    this.background();
+    this.drawFrame(0,0,165,39)
+    this.playerMovement();
+  }
+  
+  xPos=165;
+  yPos=50;
+  
+  document.addEventListener('keydown',playerMovement) 
+  
+  function playerMovement(e){
+    var x = e.keyCode;
     
-    xPos-=10;
-  }
-
-  if(x == 38){
-    yPos-=10;
-  }
-
-  if(x == 40){
-    yPos+=10;
-  }
-  console.log(x);
+    if (x == 37 || 38 || 39 || 40){
+      this.x=xPos;
+      this.y=yPos;
+      ctx.clearRect(this.x, this.y, player.width, player.height);
+      console.log(this.x,this.y)      
+  
+    if(x == 39){
+      //right
+      xPos+=16;
+    }
+  
+    if(x == 37){
+      //left
+      xPos-=16;
+    }
+  
+    if(x == 38){
+      //up
+      yPos-=9;
+    }
+  
+    if(x == 40){
+      //down
+      e.preventDefault();
+      yPos+=9;
+    }
+    console.log(x);
+    drawFrame(0,0, xPos, yPos);
+  };
 }
-
-document.onkeydown = playerMovement;
+//ctx.clearRect(player.x, player.y, player.width, player.height);
+  document.onkeydown = playerMovement;
+  //document.onkeyup = playerMovement
+  
